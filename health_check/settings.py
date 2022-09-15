3e7007c362c27f56e7721be3bdeb6c8dd99aa30c
alihajiali <alihajiali1379@gmail.com>
2022-09-16 01:23:56 +0430

    push finally

--- health_check/settings.py
+++ health_check/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from config import *
+from configs import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
